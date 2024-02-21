701bec26ea61999e0b82ecd67355bc1af77ad7d6
sybock <sybock@snu.ac.kr>
2024-02-21 12:33:11 +0900

    feat: 가상계좌 루비 코드 수정

--- payment-virtualaccount-api/rubyonrails/app/controllers/payment_controller.rb
+++ payment-virtualaccount-api/rubyonrails/app/controllers/payment_controller.rb
@@ -3,16 +3,26 @@ class PaymentController < ApplicationController
   def inquiryapi
     datetime = Time.now.to_i
     paymentKey = "q2Dv9ZPM7zXLkKEypNArWkaKzB9KArlmeaxYG5R4Jo6bnOwB"
+
+    amount = 50000
+    orderId
       
     options = {
       headers: {
         Authorization: "Basic " + Base64.strict_encode64("test_sk_D4yKeq5bgrpKRd0JYbLVGX0lzW6Y:"),
         "Content-Type": "application/json"
+      },
+      body: {
+        orderId: datetime,
+        amount: 50000,
+        customerEmail: "customer@email.com",
+        orderName: "토스 가상계좌 결제",
+        bank: "우리"
       }
     }
       
 	begin
-      response = HTTParty.get("https://api.tosspayments.com/v1/payments/" + paymentKey, options).parsed_response
+      response = HTTParty.get("https://api.tosspayments.com/v1/virtual-accounts", options).parsed_response
       @Response = response
     end      
   end
